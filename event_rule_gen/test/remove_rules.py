6109b1f39573cb901ccb43eaff660b87f26e20df
Rishang <rishangbhavsarcs@gmail.com>
2021-10-26 11:06:42 +0530

    temp

--- event_rule_gen/test/remove_rules.py
+++ event_rule_gen/test/remove_rules.py
@@ -1,7 +1,28 @@
 import boto3
+import json
+import time
 
 sess = boto3.session.Session(profile_name='electromech',region_name='us-east-1')
 
+def list_lambda_triggers(FunctionName):
+    client = sess.client("lambda")
+    try:
+        resp = client.get_policy(
+            FunctionName=FunctionName,
+        )
+        return json.loads(resp["Policy"])
+    except client.exceptions.ResourceNotFoundException:
+        return {}
+
+
+def detach_lambda_rule(function_name, statement_id):
+    client = sess.client("lambda")
+
+    response = client.remove_permission(
+        FunctionName=function_name,
+        StatementId=statement_id,
+    )
+    return response
 
 
 def del_rule(prefix):
@@ -18,6 +39,20 @@ def del_rule(prefix):
 
     return response
 
-x=del_rule('event-scale')
 
-print(x)
+def remove_attached_rules(attached_function:str, rule_prefix:str):
+    t = list_lambda_triggers(attached_function)
+    if t.get('Statement'):
+        for sid in t['Statement']:
+            print(sid['Sid'])
+            detach_lambda_rule(attached_function, sid['Sid'])
+    time.sleep(3)
+    del_rule(prefix=rule_prefix)
+
+# x=del_rule('event-scale')
+
+t = list_lambda_triggers('hi')
+if t.get('Statement'):
+    for sid in t['Statement']:
+        print(sid['Sid'])
+        detach_lambda_rule('hi', sid['Sid'])
